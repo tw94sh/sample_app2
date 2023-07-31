228f59bf0110bb83374c4e11c47bf32e831209be
tw94sh <dsph2011@gmail.com>
2023-07-31 19:00:27 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
+class StaticPagesControllerTest <
+ActionDispatch::IntegrationTest
+    test "should get root" do
+        get static_pages_home_url
+        assert_response :success
+    end
+    test "should get home" do
+        get static_pages_home_url
+        assert_response :success
+        assert_select "title", "Home | #{@base_title}"
+    end
+    test "should get help" do
+        get static_pages_help_url
+        assert_response :success
+        assert_select "title", "Help |#{@base_title}"
+    end
+    test "should get about" do
+        assert_select "title", "About | #{@base_title}"
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-  end
+    end
 end
